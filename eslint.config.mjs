49f73657b28b32370a824cfcf94d9afe06dd5976
redfoxes8 <redfoxes333@gmail.com>
2026-04-19 10:53:42 +0300

    chore/setup-storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config"
 import nextConfig from "eslint-config-next"
 
-const eslintConfig = defineConfig([...nextConfig, globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"])])
+const eslintConfig = defineConfig([
+  ...nextConfig,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  ...storybook.configs["flat/recommended"]
+])
 
 export default eslintConfig
