8363aad1f32da7adf31bfeccbbed727cd8f26808
oshknay5 <oshknay82@gmail.com>
2026-06-27 00:04:53 +0300

    IN-117 feat(routing): add profile page routes and update main page

--- src/app/(user)/profile/[userId]/page.tsx
+++ src/app/(user)/profile/[userId]/page.tsx
@@ -1,9 +1,13 @@
-"use client"
+import { ProfilePage } from "@/screens/profile-page/ProfilePage"
 
-export default function Profile() {
-  return (
-    <div style={{ paddingTop: "80px", paddingLeft: "300px" }}>
-      <h1 style={{ color: "red" }}>Profile</h1>
-    </div>
-  )
+type Props = {
+  params: Promise<{ userId: string }>
+  searchParams: Promise<{ postId?: string }>
+}
+
+export default async function Page({ params, searchParams }: Props) {
+  const { userId } = await params
+  const { postId } = await searchParams
+
+  return <ProfilePage userId={userId} postId={postId} />
 }
