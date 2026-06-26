a9efe04281a5f293664e5f9499448cc35f7c3eea
oshknay5 <oshknay82@gmail.com>
2026-06-26 23:36:55 +0300

    IN-117 chore(config): update dependencies and environment configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images-575589967758-eu-central-1-an.s3.eu-central-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   output: "standalone",
   /* config options here */
   reactCompiler: true,
